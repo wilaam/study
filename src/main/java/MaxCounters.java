052c62295fa5f70768bc42be0951dacfdcf3f05c
wilaam <wilam@tlen.pl>
2016-01-10 11:35:25 +0100

    MaxCounters - performance improvments.

--- src/main/java/MaxCounters.java
+++ src/main/java/MaxCounters.java
@@ -12,19 +12,28 @@ class Solution5 {
     public int[] solution5(int N, int[] A) {
         int counters[] = new int[N];
         int maxCounter = 0;
+        int minValue = 0;
 
         for (int i = 0; i < A.length; i++) {
             if (A[i] >= 1 && A[i] <= N) {
-                counters[A[i] - 1] = counters[A[i] - 1] + 1;
+                if (counters[A[i] - 1] < minValue) {
+                    counters[A[i] - 1] = minValue;
+                }
+                counters[A[i] - 1] = (counters[A[i] - 1] + 1);
                 if (counters[A[i] - 1] > maxCounter) {
                     maxCounter = counters[A[i] - 1];
                 }
             } else if (A[i] > N) {
-                for (int j = 0; j < counters.length; j++) {
-                    counters[j] = maxCounter;
-                }
+                minValue = maxCounter;
             }
         }
+
+        for (int i = 0; i < counters.length; i++) {
+            if (counters[i] < minValue) {
+                counters[i] = minValue;
+            }
+        }
+
         for (int i = 0; i < counters.length; i++) {
             System.out.print(counters[i]);
         }
