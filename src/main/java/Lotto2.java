10c39ee29e6f0921e31af76b06a7a215f47ce8d4
Adam Stawicki <adam.stawicki@tomtom.com>
2016-01-13 18:28:59 +0100

    lotto2

--- src/main/java/Lotto2.java
+++ src/main/java/Lotto2.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
@@ -7,13 +10,29 @@ import java.util.Scanner;
  */
 public class Lotto2 {
 
-    public static void main(String[] args) {
+    static String name;
+
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.println("Program lotto");
         System.out.println("Ile razy chcesz wylosować liczby 1 z 49 ");
         Scanner input = new Scanner(System.in);
         String text = input.nextLine();
         int amount = Integer.parseInt(text);
 
+        int[][] lottoResult = drawALots(amount);
+
+        //todo: extract printing to new method
+        for (int i = 0; i < lottoResult.length; i++) {
+            for (int j = 0; j < lottoResult[i].length; j++) {
+                System.out.print(lottoResult[i][j] + ",");
+            }
+        }
+        PrintStream out = new PrintStream("./test.txt");
+        out.println("hello");
+        out.flush();
+    }
+
+    private static int[][] drawALots(int amount) {
         Random random = new Random();
         int[][] lottoResult = new int[amount][6];
 
@@ -25,13 +44,7 @@ public class Lotto2 {
             }
             Arrays.sort(lottoResult[i]);
         }
-
-        for (int i = 0; i < lottoResult.length; i++) {
-            for (int j = 0; j < lottoResult[i].length; j++) {
-                System.out.print(lottoResult[i][j] + ",");
-            }
-            System.out.println("");
-        }
+        return lottoResult;
     }
 
     private static boolean contains(int[] lottoResult, int j) {
