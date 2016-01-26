1e5dd4d4ef78961d5f717bcccec81d0fba1943da
wilaam <wilam@tlen.pl>
2016-01-26 18:39:58 +0100

    Find - added method containsText

--- src/main/java/Find.java
+++ src/main/java/Find.java
@@ -1,33 +1,49 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 /**
  * Created by stawicad on 2016-01-13.
  */
 public class Find {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         File currentFile = new File(args[0]);
         String nameValue = null;
+        String containsValue = null;
+
         if (args.length > 1 && args[1].equals("-name")) {
             nameValue = args[2];
         }
-        
-        printFile(currentFile, nameValue);
+        if (args.length > 1 && args[3].equals("-contains")) {
+            containsValue = args[4];
+        }
+        printFile(currentFile, nameValue, containsValue);
     }
 
-    private static void printFile(File currentFile, String nameValue) {
+    private static void printFile(File currentFile, String nameValue, String containsValue) throws FileNotFoundException {
         for (int i = 0; i < currentFile.listFiles().length; i++) {
             File file = currentFile.listFiles()[i];
             if (nameValue == null) {
                 System.out.println(file);
-            }
-            else {
-                if (file.getName().equals(nameValue)) {
+            } else {
+                if (file.getName().equals(nameValue) && (containsText(currentFile, containsValue))) {
                     System.out.println(file);
                 }
             }
             if (file.isDirectory()) {
-                printFile(file, nameValue);
+                printFile(file, nameValue, containsValue);
             }
         }
+    }
+
+    private static boolean containsText(File currentFile, String containsValue) throws FileNotFoundException {
+        Scanner scanner = new Scanner(currentFile);
+        while (scanner.hasNext()) {
+            String textInLine = scanner.nextLine();
+            if (textInLine.contains(containsValue)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
