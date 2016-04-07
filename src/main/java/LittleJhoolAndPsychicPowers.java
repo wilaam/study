3c7065db85aceb269fc8609a8c00a0a868ea155c
Adam Stawicki <adam.stawicki@tomtom.com>
2016-04-07 10:21:30 +0200

    zajecia

--- src/main/java/LittleJhoolAndPsychicPowers.java
+++ src/main/java/LittleJhoolAndPsychicPowers.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class LittleJhoolAndPsychicPowers {
     public static void main(String[] args) {
-        System.out.println("Please add binary number");
+//        System.out.println("Please add binary number");
         Scanner input = new Scanner(System.in);
         String text = input.nextLine();
 
@@ -13,13 +13,17 @@ public class LittleJhoolAndPsychicPowers {
         for (int i = 0; i < binaryNumber.length; i++) {
             binaryNumber[i] = text;
             for (int j = 1; j < binaryNumber[i].length(); j++) {
-                if (binaryNumber[i].charAt(j) == '1') {
+                if (binaryNumber[i].charAt(j) == '1' || binaryNumber[i].charAt(j) == '0') {
                     if (binaryNumber[i].charAt(j) == binaryNumber[i].charAt(j - 1)) {
                         count++;
+                        if (count>=6){
+                            break;
+                        }
                     }
+                    else count=1;
                 }
             }
-            if (count == 6) {
+            if (count >= 6) {
                 System.out.println("Sorry, sorry!");
             } else System.out.println("Good luck!");
         }
