5b54aae246c3ae6518b0d0d34d992c80b7aeb763
wilaam <wilam@tlen.pl>
2016-08-09 22:28:46 +0200

    File Statistics2 - added solution without groupid

---
+++ src/main/java/FileStatistic2.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class FileStatistic2 {
+    public static void main(String[] args) throws IOException{
+
+        Scanner scanner = new Scanner(System.in);
+        String inputFile = scanner.next();
+        File file = new File(inputFile);
+        Scanner inFile = new Scanner(file);
+
+        int lines = 0;
+        int words = 0;
+        int characters = 0;
+
+        while(inFile.hasNextLine()){
+            lines++;
+            String line = inFile.nextLine();
+            for (int i = 0; i <line.length() ; i++) {
+                if (line.charAt(i)!=' ' && line.charAt(i)!='\n'){
+                    characters++;
+                }
+            }
+            words+=new StringTokenizer(line,",;:.").countTokens();
+        }
+
+        System.out.println(lines);
+        System.out.println(words);
+        System.out.println(characters);
+        System.out.println(Files.getOwner(file.toPath(), LinkOption.NOFOLLOW_LINKS));
+
+    }
+}
+
