59f5c524c918fa8ae61931dc9cceae53857d2905
wilaam <wilam@tlen.pl>
2016-08-18 10:28:10 +0200

    File Statistics2 - added mechanism for checking last file modification

--- src/main/java/FileStatistic2.java
+++ src/main/java/FileStatistic2.java
@@ -2,12 +2,14 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class FileStatistic2 {
     public static void main(String[] args) throws IOException{
 
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         Scanner scanner = new Scanner(System.in);
         String inputFile = scanner.next();
         File file = new File(inputFile);
@@ -20,7 +22,7 @@ public class FileStatistic2 {
         while(inFile.hasNextLine()){
             lines++;
             String line = inFile.nextLine();
-            for (int i = 0; i <line.length() ; i++) {
+            for (int i = 0; i <line.length(); i++) {
                 if (line.charAt(i)!=' ' && line.charAt(i)!='\n'){
                     characters++;
                 }
@@ -32,7 +34,9 @@ public class FileStatistic2 {
         System.out.println(words);
         System.out.println(characters);
         System.out.println(Files.getOwner(file.toPath(), LinkOption.NOFOLLOW_LINKS));
-
+        System.out.println("1000");
+        System.out.println(file.lastModified());
+        //System.out.println(sdf.format(file.lastModified()));
     }
 }
 
